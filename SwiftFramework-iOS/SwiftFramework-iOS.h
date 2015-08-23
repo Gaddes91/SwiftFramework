77061d1939622469a5ee62c4fa70538a90d44c8b
Matthew Gaddes <matthew.gaddes@gmail.com>
2015-08-23 10:24:07 +0100

    Initial commit

---
+++ SwiftFramework-iOS/SwiftFramework-iOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFramework-iOS.h
+//  SwiftFramework-iOS
+//
+//  Created by Matthew Gaddes on 23/08/2015.
+//  Copyright (c) 2015 Matthew Gaddes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFramework-iOS.
+FOUNDATION_EXPORT double SwiftFramework_iOSVersionNumber;
+
+//! Project version string for SwiftFramework-iOS.
+FOUNDATION_EXPORT const unsigned char SwiftFramework_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFramework_iOS/PublicHeader.h>
+
+
