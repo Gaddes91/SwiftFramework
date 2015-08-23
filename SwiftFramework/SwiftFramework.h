3544e9fb3d3d462a1907d6c8104d4ab90b2dccfb
Matthew Gaddes <matthew.gaddes@gmail.com>
2015-08-23 10:11:46 +0100

    Initial Commit

---
+++ SwiftFramework/SwiftFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFramework.h
+//  SwiftFramework
+//
+//  Created by Matthew Gaddes on 23/08/2015.
+//  Copyright (c) 2015 Matthew Gaddes. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftFramework.
+FOUNDATION_EXPORT double SwiftFrameworkVersionNumber;
+
+//! Project version string for SwiftFramework.
+FOUNDATION_EXPORT const unsigned char SwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFramework/PublicHeader.h>
+
+
